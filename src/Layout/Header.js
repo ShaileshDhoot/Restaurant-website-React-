a2af269b7db6314c7becc9276dc0e7d5bccea328
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-09-05 13:55:27 +0530

    listing menu items with description price

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,15 +1,21 @@
-import React from "react";
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../Images/meals.jpg'
 import classes from './Header.module.css';
-import CartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-    return <>
-        <header className={classes.header}>
-            <h2>Reactmeals</h2>
-            <CartButton />
-        </header>
-        <div></div>
-    </>
-}
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
